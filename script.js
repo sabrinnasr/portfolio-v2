96f8bea65b1f58f987fcb26c4e6a22607b75fb94
Sabrina Rodrigues <sabrinnasrodrigues@gmail.com>
2024-12-29 23:17:54 -0500

    started to add responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.onclick = () => {
+    navLinks.classList.toggle('active')
+}
+
+
 function scrollToTop() {
     window.scrollTo({
         top: 0,
